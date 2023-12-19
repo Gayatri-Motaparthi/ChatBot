e8ef82bec0ca3d3d28c281111c586e365e3c4d0c
Gayatri Motaparthi <smot0052@uni.sydney.edu.au>
2023-12-19 16:37:54 +0530

    user can now select a theme

--- routes/home.js
+++ routes/home.js
@@ -6,30 +6,37 @@ const path = require("path");
 // const Hashing = require("./hashing");
 // const hash = new Hashing();
 
+var theme;
+
 
 
 router.get("/", function (req, res) {
+    res.render("theme", { theme });
 
-    if ("myCookie" in req.cookies) {
-        console.log(req.cookies.myCookie)
-        res.render("homepage")
-    }
-    else {
-        res.render("preLogin");
-    }
+});
+
+router.post("/", function (req, res) {
+
+    theme = req.body.theme
+    console.log(theme);
+    res.redirect("/preLogin");
+
+});
+
+router.get("/preLogin", function (req, res) {
+    res.render("preLogin", { theme });
 });
 
 router.get("/signup/:error?", function (req, res) {
     const error = req.params.error || "";
-    res.render("signUp", { errorMessage: error });
-
+    res.render("signUp", { theme, errorMessage: error });
 
 });
 
 
 router.get("/login/:error?", function (req, res) {
     const error = req.params.error || "";
-    res.render("login", { errorMessage: error });
+    res.render("login", { theme, errorMessage: error });
 
 });
 
@@ -37,7 +44,7 @@ router.get("/homepage/:name?", function (req, res) {
     const name = req.params.name || "";
     console.log("in the homepage request");
 
-    res.render('homepage', { userName: name, content: "" });
+    res.render('homepage', { theme, userName: name, content: "" });
 });
 
 router.post("/homepage", function (req, res) {

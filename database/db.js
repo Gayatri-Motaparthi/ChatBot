4d436b6dd1307d18f0bc2afb457e503415908078
Gayatri Motaparthi <smot0052@uni.sydney.edu.au>
2023-12-22 16:44:32 +0530

    program works as expected

--- database/db.js
+++ database/db.js
@@ -1,22 +1,13 @@
 const mongoose = require('mongoose');
 
-// Your MongoDB connection string
-// const connectionString = 'mongodb+srv://gayatriMotaparthi:LearnFlask123@cluster0.l8cnhl0.mongodb.net/db?retryWrites=true&w=majority/chatbot';
 const connectionString = 'mongodb+srv://Gayatri:Gayatri@cluster0.knoey6m.mongodb.net/';
-
-
-// Create the Mongoose connection instance
-// const db = mongoose.createConnection(connectionString, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
 
 let db = mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-db = mongoose.connection;
+db = mongoose.connection
 
 // Event listeners for connection events
 db.once('open', () => {
